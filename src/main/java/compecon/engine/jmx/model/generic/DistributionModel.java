107556ad17d3786e9fd92326f38f16ce7775ea0d
uwol <u.wol@uni-muenster.de>
2013-08-12 20:10:57 +0200

    refactoring of lorenz curve

--- src/main/java/compecon/engine/jmx/model/generic/DistributionModel.java
+++ src/main/java/compecon/engine/jmx/model/generic/DistributionModel.java
@@ -21,6 +21,12 @@ public class DistributionModel<T> extends Model {
 
 		public double[] originalValues;
 
+		// F(X)
+		public double[] ySumAtPercentOfX = new double[20];
+
+		// F(infinite)
+		public double yTotalSum;
+
 		// y-value
 		public double quantil5Percent;
 
@@ -50,28 +56,6 @@ public class DistributionModel<T> extends Model {
 		public int xWith80PercentY;
 
 		public int xWith90PercentY;
-
-		public double ySumUnder10PercentX;
-
-		public double ySumUnder20PercentX;
-
-		public double ySumUnder30PercentX;
-
-		public double ySumUnder40PercentX;
-
-		public double ySumUnder50PercentX;
-
-		public double ySumUnder60PercentX;
-
-		public double ySumUnder70PercentX;
-
-		public double ySumUnder80PercentX;
-
-		public double ySumUnder90PercentX;
-
-		// F()
-		public double totalSum;
-
 	}
 
 	protected T[] types;
@@ -137,7 +121,10 @@ public class DistributionModel<T> extends Model {
 			summaryStatisticalData.quantil95Percent = valuesAsArray[(int) (valuesAsArray.length * 0.95)];
 			summaryStatisticalData.quantil99Percent = valuesAsArray[(int) (valuesAsArray.length * 0.99)];
 			for (double value : valuesAsArray)
-				summaryStatisticalData.totalSum += value;
+				summaryStatisticalData.yTotalSum += value;
+
+			int bucketWidth = valuesAsArray.length
+					/ summaryStatisticalData.ySumAtPercentOfX.length;
 
 			double sum = 0;
 			for (int i = 0; i < valuesAsArray.length; i++) {
@@ -145,64 +132,43 @@ public class DistributionModel<T> extends Model {
 				 * xWith..PercentY
 				 */
 				sum += valuesAsArray[i];
-				if (sum > (summaryStatisticalData.totalSum * 0.9)
+				if (sum > (summaryStatisticalData.yTotalSum * 0.9)
 						&& summaryStatisticalData.xWith90PercentY == 0)
 					summaryStatisticalData.xWith90PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.8)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.8)
 						&& summaryStatisticalData.xWith80PercentY == 0)
 					summaryStatisticalData.xWith80PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.7)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.7)
 						&& summaryStatisticalData.xWith70PercentY == 0)
 					summaryStatisticalData.xWith70PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.6)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.6)
 						&& summaryStatisticalData.xWith60PercentY == 0)
 					summaryStatisticalData.xWith60PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.5)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.5)
 						&& summaryStatisticalData.xWith50PercentY == 0)
 					summaryStatisticalData.xWith50PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.4)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.4)
 						&& summaryStatisticalData.xWith40PercentY == 0)
 					summaryStatisticalData.xWith40PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.3)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.3)
 						&& summaryStatisticalData.xWith30PercentY == 0)
 					summaryStatisticalData.xWith30PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.2)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.2)
 						&& summaryStatisticalData.xWith20PercentY == 0)
 					summaryStatisticalData.xWith20PercentY = i;
-				else if (sum > (summaryStatisticalData.totalSum * 0.1)
+				else if (sum > (summaryStatisticalData.yTotalSum * 0.1)
 						&& summaryStatisticalData.xWith10PercentY == 0)
 					summaryStatisticalData.xWith10PercentY = i;
 
 				/*
-				 * ySumUnder..PercentX
+				 * ySumAtPercentOfX
 				 */
-				int position10PercentOfX = (int) (valuesAsArray.length * 0.1);
-				int position20PercentOfX = (int) (valuesAsArray.length * 0.2);
-				int position30PercentOfX = (int) (valuesAsArray.length * 0.3);
-				int position40PercentOfX = (int) (valuesAsArray.length * 0.4);
-				int position50PercentOfX = (int) (valuesAsArray.length * 0.5);
-				int position60PercentOfX = (int) (valuesAsArray.length * 0.6);
-				int position70PercentOfX = (int) (valuesAsArray.length * 0.7);
-				int position80PercentOfX = (int) (valuesAsArray.length * 0.8);
-				int position90PercentOfX = (int) (valuesAsArray.length * 0.9);
-				if (i == position10PercentOfX)
-					summaryStatisticalData.ySumUnder10PercentX = sum;
-				else if (i == position20PercentOfX)
-					summaryStatisticalData.ySumUnder20PercentX = sum;
-				else if (i == position30PercentOfX)
-					summaryStatisticalData.ySumUnder30PercentX = sum;
-				else if (i == position40PercentOfX)
-					summaryStatisticalData.ySumUnder40PercentX = sum;
-				else if (i == position50PercentOfX)
-					summaryStatisticalData.ySumUnder50PercentX = sum;
-				else if (i == position60PercentOfX)
-					summaryStatisticalData.ySumUnder60PercentX = sum;
-				else if (i == position70PercentOfX)
-					summaryStatisticalData.ySumUnder70PercentX = sum;
-				else if (i == position80PercentOfX)
-					summaryStatisticalData.ySumUnder80PercentX = sum;
-				else if (i == position90PercentOfX)
-					summaryStatisticalData.ySumUnder90PercentX = sum;
+				if (i % bucketWidth == 0) {
+					int position = i / bucketWidth;
+					if (position < summaryStatisticalData.ySumAtPercentOfX.length) {
+						summaryStatisticalData.ySumAtPercentOfX[position] = sum;
+					}
+				}
 			}
 
 			/*
@@ -216,29 +182,15 @@ public class DistributionModel<T> extends Model {
 			/*
 			 * create dataset for lorenz curve
 			 */
-			double totalSum = summaryStatisticalData.totalSum;
-
 			final XYSeries seriesLorenzCurve = new XYSeries("Lorenz curve "
 					+ type.toString());
-			seriesLorenzCurve.add(0, 0);
-			seriesLorenzCurve.add(0.1,
-					summaryStatisticalData.ySumUnder10PercentX / totalSum);
-			seriesLorenzCurve.add(0.2,
-					summaryStatisticalData.ySumUnder20PercentX / totalSum);
-			seriesLorenzCurve.add(0.3,
-					summaryStatisticalData.ySumUnder30PercentX / totalSum);
-			seriesLorenzCurve.add(0.4,
-					summaryStatisticalData.ySumUnder40PercentX / totalSum);
-			seriesLorenzCurve.add(0.5,
-					summaryStatisticalData.ySumUnder50PercentX / totalSum);
-			seriesLorenzCurve.add(0.6,
-					summaryStatisticalData.ySumUnder60PercentX / totalSum);
-			seriesLorenzCurve.add(0.7,
-					summaryStatisticalData.ySumUnder70PercentX / totalSum);
-			seriesLorenzCurve.add(0.8,
-					summaryStatisticalData.ySumUnder80PercentX / totalSum);
-			seriesLorenzCurve.add(0.9,
-					summaryStatisticalData.ySumUnder90PercentX / totalSum);
+			for (int i = 0; i < summaryStatisticalData.ySumAtPercentOfX.length; i++) {
+				double x = i
+						/ (double) summaryStatisticalData.ySumAtPercentOfX.length;
+				double y = summaryStatisticalData.ySumAtPercentOfX[i]
+						/ (double) summaryStatisticalData.yTotalSum;
+				seriesLorenzCurve.add(x, y);
+			}
 			seriesLorenzCurve.add(1, 1);
 
 			final XYSeries seriesLine = new XYSeries("line of equality "
