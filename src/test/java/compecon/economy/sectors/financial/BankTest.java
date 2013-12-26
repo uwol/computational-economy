523fe5383c8719709f9e26d57d177d9447bf4ccf
uwol <u.wol@uni-muenster.de>
2013-12-26 21:33:19 +0100

    test case for credit bank deconstruction

--- src/test/java/compecon/economy/sectors/financial/BankTest.java
+++ src/test/java/compecon/economy/sectors/financial/BankTest.java
@@ -20,13 +20,19 @@ along with ComputationalEconomy. If not, see <http://www.gnu.org/licenses/>.
 package compecon.economy.sectors.financial;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import compecon.CompEconTestSupport;
+import compecon.economy.sectors.financial.BankAccount.MoneyType;
+import compecon.economy.sectors.financial.BankAccount.TermType;
 import compecon.economy.sectors.household.Household;
+import compecon.economy.sectors.household.impl.HouseholdImpl;
 import compecon.engine.applicationcontext.ApplicationContext;
 
 public class BankTest extends CompEconTestSupport {
@@ -52,12 +58,15 @@ public class BankTest extends CompEconTestSupport {
 
 		assertEquals(0.0, household1_EUR.getBankAccountTransactionsDelegate()
 				.getBankAccount().getBalance(), epsilon);
+		assertEquals(0.0, household2_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getBalance(), epsilon);
 
 		Bank source = household1_EUR.getBankAccountTransactionsDelegate()
 				.getBankAccount().getManagingBank();
 		Bank target = household2_EUR.getBankAccountTransactionsDelegate()
 				.getBankAccount().getManagingBank();
 
+		// transfer money
 		for (int i = 1; i < 1000; i++) {
 			source.transferMoney(household1_EUR
 					.getBankAccountTransactionsDelegate().getBankAccount(),
@@ -70,5 +79,75 @@ public class BankTest extends CompEconTestSupport {
 					.getBankAccountTransactionsDelegate().getBankAccount()
 					.getBalance(), epsilon);
 		}
+	}
+
+	@Test
+	public void testCreditBankDeconstruction() {
+		Currency currency = Currency.EURO;
+
+		// init household 1
+		Household household1_EUR = ApplicationContext.getInstance()
+				.getHouseholdDAO().findAllByCurrency(currency).get(0);
+		CreditBank creditBank1_EUR = ApplicationContext.getInstance()
+				.getCreditBankDAO().findAllByCurrency(currency).get(0);
+		BankAccount bankAccount1_EUR = creditBank1_EUR.openBankAccount(
+				household1_EUR, currency, true, "transactions",
+				TermType.SHORT_TERM, MoneyType.DEPOSITS);
+		((HouseholdImpl) household1_EUR)
+				.setBankAccountTransactions(bankAccount1_EUR);
+
+		assertEquals(0.0, household1_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getBalance(), epsilon);
+
+		// init household 2
+		Household household2_EUR = ApplicationContext.getInstance()
+				.getHouseholdDAO().findAllByCurrency(currency).get(1);
+		CreditBank creditBank2_EUR = ApplicationContext.getInstance()
+				.getCreditBankDAO().findAllByCurrency(currency).get(1);
+		BankAccount bankAccount2_EUR = creditBank2_EUR.openBankAccount(
+				household2_EUR, currency, true, "transactions",
+				TermType.SHORT_TERM, MoneyType.DEPOSITS);
+		((HouseholdImpl) household2_EUR)
+				.setBankAccountTransactions(bankAccount2_EUR);
+
+		assertEquals(0.0, household2_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getBalance(), epsilon);
+
+		// transfer money
+		creditBank1_EUR.transferMoney(household1_EUR
+				.getBankAccountTransactionsDelegate().getBankAccount(),
+				household2_EUR.getBankAccountTransactionsDelegate()
+						.getBankAccount(), 10, "Transaction");
+
+		assertEquals(-10.0, household1_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getBalance(), epsilon);
+		assertEquals(10.0, household2_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getBalance(), epsilon);
+		assertNotNull(((HouseholdImpl) household2_EUR)
+				.getBankAccountTransactions());
+
+		// deconstruct credit bank 2
+		creditBank2_EUR.deconstruct();
+
+		/*
+		 * credit bank 2 informs every customer of its deconstruction ->
+		 * household2_EUR should have reset its transaction bank account
+		 */
+		assertNull(((HouseholdImpl) household2_EUR)
+				.getBankAccountTransactions());
+
+		// the bank account delegate is called again -> a new bank account
+		// should be created
+		household2_EUR.getBankAccountTransactionsDelegate().getBankAccount();
+		assertNotNull(((HouseholdImpl) household2_EUR)
+				.getBankAccountTransactions());
+
+		// but at a different credit bank
+		assertNotSame(household2_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getManagingBank(), creditBank2_EUR);
+
+		// the new bank account has a balance of 0.0 EUR, instead of 10.0 EUR
+		assertEquals(0.0, household2_EUR.getBankAccountTransactionsDelegate()
+				.getBankAccount().getBalance(), epsilon);
 	}
 }
